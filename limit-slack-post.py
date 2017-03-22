2404b65d4514614ce53537682f0aa058bafd6a84
Mency Woo <mency.woo@gmail.com>
2017-03-21 20:41:10 -0700

    Update time to be time of message; add env var checking as per Scott's suggestions

--- limit-slack-post.py
+++ limit-slack-post.py
@@ -5,18 +5,21 @@ import json
 import logging
 import os
 import json
-from datetime import datetime
+from dateutil import parser
 
 from base64 import b64decode
 from urllib2 import Request, urlopen, URLError, HTTPError
 
+
+if not 'kmsEncryptedHookUrl' in os.environ.keys() or  not 'slackChannel' in os.environ.keys() or not 'slackUser' in os.environ.keys():
+    print ("Environment variables not defined.  Need 'kmsEncryptedHookUrl', 'slackChannel' and 'slackUser' ")
+    exit(1)
 
 # The base-64 encoded, encrypted key (CiphertextBlob) stored in the kmsEncryptedHookUrl environment variable
 ENCRYPTED_HOOK_URL = os.environ['kmsEncryptedHookUrl']
 # The Slack channel to send a message to stored in the slackChannel environment variable
 SLACK_CHANNEL = os.environ['slackChannel']
 SLACK_USER = os.environ['slackUser']
-
 if 'post_start' in os.environ.keys():
     POST_START=os.environ['post_start']
 else:
@@ -25,7 +28,7 @@ else:
 if 'post_stop' in os.environ.keys():
     POST_STOP=os.environ['post_stop']
 else:
-    POST_STOP=15
+    POST_STOP=17
 
 HOOK_URL = "https://" + boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_HOOK_URL))['Plaintext']
 
@@ -34,16 +37,17 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 ## no slack messages on Sat/Sun
-def slacktime():
-    currHour = datetime.now().hour
-    currDay = datetime.today().weekday()
-    return currHour >= POST_START and currHour <= POST_STOP and currDay < 5
+def slacktime(timestamp):
+    msg_hour = parser.parse(timestamp).hour
+    logger.info ("Hour: " + str(msg_hour))
+    return (msg_hour >= POST_START and msg_hour <= POST_STOP )
 
 def lambda_handler(event, context):
     logger.info("Event: " + str(event))
     message = event['Records'][0]['Sns']['Message']
+    timestamp = event['Records'][0]['Sns']['Timestamp']
 
-    if slacktime() is False:
+    if slacktime(timestamp) is False:
         logger.info("Current time falls outside of Slack posting hours")
         return
 
